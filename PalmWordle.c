1c4807b7d2b803fea3ae08d19858f4a0689818dc
Robbie Nesmith <bobberto1995@gmail.com>
2022-02-01 07:15:38 -0600

    Handle keyboard input

--- PalmWordle.c
+++ PalmWordle.c
@@ -26,6 +26,16 @@ UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags)
 	UInt32 nowDays;
 	UInt32 seedDays = 42904; // June 19 2021
 	UInt32 wordIndex;
+	int row, col, i, j;
+	int guessRow = 0;
+	int guessCol = 0;
+	Char guess[6][5];
+	RGBColorType rgb;
+	RectangleType rect;
+	rect.topLeft.x = 10;
+	rect.topLeft.y = 10;
+	rect.extent.x = 20;
+	rect.extent.y = 20;
 
 	if (cmd == sysAppLaunchCmdNormalLaunch) // Make sure only react to NormalLaunch, not Reset, Beam, Find, GoTo...
 	{
@@ -46,8 +56,6 @@ UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags)
 			switch (e.eType)
 			{
 			case ctlSelectEvent:
-				if (e.data.ctlSelect.controlID == Ok)
-					goto _quit;
 				goto _default;
 				break;
 
@@ -58,7 +66,85 @@ UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags)
 			case frmOpenEvent:
 				pfrm = FrmGetActiveForm();
 				FrmDrawForm(pfrm);
-				WinDrawChars(word, 5, 50, 50);
+				for (row = 0; row < 6; row++)
+				{
+					for (col = 0; col < 5; col++)
+					{
+						rect.topLeft.x = col * 24 + 20;
+						rect.topLeft.y = row * 24 + 18;
+						WinDrawRectangleFrame(simpleFrame, &rect);
+					}
+				}
+				break;
+
+			case keyDownEvent:
+				if (e.data.keyDown.chr == chrBackspace)
+				{
+					if (guessCol > 0)
+					{
+						guessCol--;
+					}
+					rect.topLeft.x = guessCol * 24 + 20;
+					rect.topLeft.y = guessRow * 24 + 18;
+					WinEraseRectangle(&rect, 0);
+				}
+				else if (e.data.keyDown.chr == chrLineFeed &&
+						 guessCol == 5)
+				{
+					for (i = 0; i < 5; i++)
+					{
+						if (guess[guessRow][i] == word[i])
+						{
+							rgb.r = 0;
+							rgb.g = 127;
+							rgb.b = 0;
+						}
+						else
+						{
+							rgb.r = 200;
+							rgb.g = 200;
+							rgb.b = 200;
+							for (j = 0; j < 5; j++)
+							{
+								if (word[j] == guess[guessRow][i])
+								{
+									rgb.r = 255;
+									rgb.g = 255;
+									rgb.b = 0;
+								}
+							}
+						}
+						rect.topLeft.x = i * 24 + 20;
+						rect.topLeft.y = guessRow * 24 + 18;
+						WinSetForeColorRGB(&rgb, NULL);
+						WinDrawRectangleFrame(0x0002, &rect);
+					}
+					if (guessRow < 6)
+					{
+						guessRow++;
+						guessCol = 0;
+					}
+					rgb.r = 0;
+					rgb.g = 0;
+					rgb.b = 0;
+					WinSetForeColorRGB(&rgb, NULL);
+				}
+				else if (((e.data.keyDown.chr >= chrCapital_A && e.data.keyDown.chr <= chrCapital_Z) ||
+						  (e.data.keyDown.chr >= chrSmall_A && e.data.keyDown.chr <= chrSmall_Z)) &&
+						 guessCol < 5)
+				{
+					if (e.data.keyDown.chr > chrCapital_Z)
+					{
+						guess[guessRow][guessCol] = e.data.keyDown.chr;
+					}
+					else
+					{
+						guess[guessRow][guessCol] = e.data.keyDown.chr + 0x20;
+					}
+					FntSetFont(largeFont);
+					WinDrawChar(guess[guessRow][guessCol] - 0x20, guessCol * 24 + 27, guessRow * 24 + 20);
+					guessCol++;
+				}
 				break;
 
 			case menuEvent:
