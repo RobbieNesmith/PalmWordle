8dae88116326069c22250197a3fb68188bb3f4f4
Robbie Nesmith <bobberto1995@gmail.com>
2022-02-01 07:38:52 -0600

    Move logic to separate event handler

--- PalmWordle.c
+++ PalmWordle.c
@@ -16,24 +16,131 @@
 #include <PalmOS.h>
 #include "wordorder.h"
 
+const Char *word;
+int guessRow = 0;
+int guessCol = 0;
+Char guess[6][5];
+
+static Boolean MainFormHandleEvent(EventPtr e)
+{
+	int i, j;
+	Boolean handled = false;
+	RGBColorType rgb;
+	RectangleType rect;
+	rect.extent.x = 20;
+	rect.extent.y = 20;
+
+	switch (e->eType)
+	{
+	case keyDownEvent:
+		if (e->data.keyDown.chr == chrBackspace)
+		{
+			if (guessCol > 0)
+			{
+				guessCol--;
+			}
+			rect.topLeft.x = guessCol * 24 + 20;
+			rect.topLeft.y = guessRow * 24 + 18;
+			WinEraseRectangle(&rect, 0);
+			handled = true;
+		}
+		else if (e->data.keyDown.chr == chrLineFeed &&
+				 guessCol == 5)
+		{
+			for (i = 0; i < 5; i++)
+			{
+				if (guess[guessRow][i] == word[i])
+				{
+					rgb.r = 0;
+					rgb.g = 127;
+					rgb.b = 0;
+				}
+				else
+				{
+					rgb.r = 200;
+					rgb.g = 200;
+					rgb.b = 200;
+					for (j = 0; j < 5; j++)
+					{
+						if (word[j] == guess[guessRow][i])
+						{
+							rgb.r = 255;
+							rgb.g = 255;
+							rgb.b = 0;
+						}
+					}
+				}
+				rect.topLeft.x = i * 24 + 20;
+				rect.topLeft.y = guessRow * 24 + 18;
+				WinSetForeColorRGB(&rgb, NULL);
+				WinDrawRectangleFrame(0x0002, &rect);
+			}
+			if (guessRow < 6)
+			{
+				guessRow++;
+				guessCol = 0;
+			}
+			rgb.r = 0;
+			rgb.g = 0;
+			rgb.b = 0;
+			WinSetForeColorRGB(&rgb, NULL);
+			handled = true;
+		}
+		else if (((e->data.keyDown.chr >= chrCapital_A && e->data.keyDown.chr <= chrCapital_Z) ||
+				  (e->data.keyDown.chr >= chrSmall_A && e->data.keyDown.chr <= chrSmall_Z)) &&
+				 guessCol < 5)
+		{
+			if (e->data.keyDown.chr > chrCapital_Z)
+			{
+				guess[guessRow][guessCol] = e->data.keyDown.chr;
+			}
+			else
+			{
+				guess[guessRow][guessCol] = e->data.keyDown.chr + 0x20;
+			}
+			FntSetFont(largeFont);
+			WinDrawChar(guess[guessRow][guessCol] - 0x20, guessCol * 24 + 27, guessRow * 24 + 20);
+			guessCol++;
+			handled = true;
+		}
+		break;
+	default:
+		//do nothing
+	}
+	return handled;
+}
+
+static Boolean ApplicationHandleEvent(EventPtr e)
+{
+	short formId;
+	FormPtr frm;
+
+	if (e->eType == frmLoadEvent)
+	{
+		formId = e->data.frmLoad.formID;
+		frm = FrmInitForm(formId);
+		FrmSetActiveForm(frm);
+
+		if (formId == Form1)
+		{
+			FrmSetEventHandler(frm, MainFormHandleEvent);
+		}
+		return true;
+	}
+	return false;
+}
+
 UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags)
 {
 	short err;
 	EventType e;
 	FormType *pfrm;
-	const Char *word;
 	UInt32 nowSeconds;
 	UInt32 nowDays;
 	UInt32 seedDays = 42904; // June 19 2021
 	UInt32 wordIndex;
-	int row, col, i, j;
-	int guessRow = 0;
-	int guessCol = 0;
-	Char guess[6][5];
-	RGBColorType rgb;
+	int row, col;
 	RectangleType rect;
-	rect.topLeft.x = 10;
-	rect.topLeft.y = 10;
 	rect.extent.x = 20;
 	rect.extent.y = 20;
 
@@ -52,6 +159,10 @@ UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags)
 				continue;
 			if (MenuHandleEvent((void *)0, &e, &err))
 				continue;
+			if (ApplicationHandleEvent(&e))
+			{
+				continue;
+			}
 
 			switch (e.eType)
 			{
@@ -66,84 +177,17 @@ UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags)
 			case frmOpenEvent:
 				pfrm = FrmGetActiveForm();
 				FrmDrawForm(pfrm);
-				for (row = 0; row < 6; row++)
+				if (pfrm->formId == Form1)
 				{
-					for (col = 0; col < 5; col++)
+					for (row = 0; row < 6; row++)
 					{
-						rect.topLeft.x = col * 24 + 20;
-						rect.topLeft.y = row * 24 + 18;
-						WinDrawRectangleFrame(simpleFrame, &rect);
-					}
-				}
-				break;
-
-			case keyDownEvent:
-				if (e.data.keyDown.chr == chrBackspace)
-				{
-					if (guessCol > 0)
-					{
-						guessCol--;
-					}
-					rect.topLeft.x = guessCol * 24 + 20;
-					rect.topLeft.y = guessRow * 24 + 18;
-					WinEraseRectangle(&rect, 0);
-				}
-				else if (e.data.keyDown.chr == chrLineFeed &&
-						 guessCol == 5)
-				{
-					for (i = 0; i < 5; i++)
-					{
-						if (guess[guessRow][i] == word[i])
+						for (col = 0; col < 5; col++)
 						{
-							rgb.r = 0;
-							rgb.g = 127;
-							rgb.b = 0;
+							rect.topLeft.x = col * 24 + 20;
+							rect.topLeft.y = row * 24 + 18;
+							WinDrawRectangleFrame(simpleFrame, &rect);
 						}
-						else
-						{
-							rgb.r = 200;
-							rgb.g = 200;
-							rgb.b = 200;
-							for (j = 0; j < 5; j++)
-							{
-								if (word[j] == guess[guessRow][i])
-								{
-									rgb.r = 255;
-									rgb.g = 255;
-									rgb.b = 0;
-								}
-							}
-						}
-						rect.topLeft.x = i * 24 + 20;
-						rect.topLeft.y = guessRow * 24 + 18;
-						WinSetForeColorRGB(&rgb, NULL);
-						WinDrawRectangleFrame(0x0002, &rect);
 					}
-					if (guessRow < 6)
-					{
-						guessRow++;
-						guessCol = 0;
-					}
-					rgb.r = 0;
-					rgb.g = 0;
-					rgb.b = 0;
-					WinSetForeColorRGB(&rgb, NULL);
-				}
-				else if (((e.data.keyDown.chr >= chrCapital_A && e.data.keyDown.chr <= chrCapital_Z) ||
-						  (e.data.keyDown.chr >= chrSmall_A && e.data.keyDown.chr <= chrSmall_Z)) &&
-						 guessCol < 5)
-				{
-					if (e.data.keyDown.chr > chrCapital_Z)
-					{
-						guess[guessRow][guessCol] = e.data.keyDown.chr;
-					}
-					else
-					{
-						guess[guessRow][guessCol] = e.data.keyDown.chr + 0x20;
-					}
-					FntSetFont(largeFont);
-					WinDrawChar(guess[guessRow][guessCol] - 0x20, guessCol * 24 + 27, guessRow * 24 + 20);
-					guessCol++;
 				}
 				break;
 
@@ -156,7 +200,7 @@ UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags)
 			default:
 			_default:
 				if (FrmGetActiveForm())
-					FrmHandleEvent(FrmGetActiveForm(), &e);
+					FrmDispatchEvent(&e);
 			}
 		}
 
